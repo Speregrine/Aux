453b7df636b25e05a2bf09b454f50a9f5fd9473d
Speregrine <38218791+Speregrine@users.noreply.github.com>
2019-10-30 15:42:36 -0400

    corrected placement of an if statement

--- main.js
+++ main.js
@@ -253,17 +253,18 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-     if (login == false)
+      if (login == false)
     {
     console.log("signin");
     document.location.href = "signin.html";
         return;
     }
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+   
   modal.style.display = "none";
 }
 
